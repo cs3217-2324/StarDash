6c01dfb48b4a4ffe42155731cefd5f55e8fb11ec
ruihan00 <ruixhan@gmail.com>
2024-03-12 02:00:03 +0800

    add assets and base blank proj

---
+++ star-dash/star-dash/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  star-dash
+//
+//  Created by Lau Rui han on 12/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
