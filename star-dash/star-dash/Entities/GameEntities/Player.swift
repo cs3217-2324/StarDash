237e55a139d4d143c56ac2487e712b336dbde69e
ruihan00 <ruixhan@gmail.com>
2024-03-12 22:29:24 +0800

    add entity implementation and collision mask

--- star-dash/star-dash/Entities/GameEntities/Player.swift
+++ star-dash/star-dash/Entities/GameEntities/Player.swift
@@ -7,10 +7,10 @@
 
 import Foundation
 class Player: Entity {
-    let id: UUID
+    let id: EntityId
     private let position: CGPoint
     private let playerSprite: PlayerSprite
-    init(id: UUID, position: CGPoint, playerSprite: PlayerSprite) {
+    init(id: EntityId, position: CGPoint, playerSprite: PlayerSprite) {
         self.id = id
         self.position = position
         self.playerSprite = playerSprite
@@ -24,7 +24,13 @@ class Player: Entity {
     func setUpAndAdd(to: EntityManager) {
         let positionComponent = PositionComponent(entityId: self.id, position: self.position, rotation: .zero)
         let healthComponent = HealthComponent(entityId: self.id, health: 100)
-        let physicsComponent = PhysicsComponent(entityId: self.id, mass: .zero, velocity: .zero, force: .zero, collisionMask: .zero, affectedByGravity: true)
+        let physicsComponent = PhysicsComponent(
+            entityId: self.id,
+            mass: .zero,
+            velocity: .zero,
+            force: .zero,
+            collisionMask: PhysicsConstants.CollisionMask.playerCollisionMask,
+            affectedByGravity: true)
         
         let spriteComponent = SpriteComponent(entityId: self.id, image: "", size: .zero)
         
@@ -32,5 +38,6 @@ class Player: Entity {
         to.add(component: positionComponent)
         to.add(component: healthComponent)
         to.add(component: physicsComponent)
+        to.add(component: spriteComponent)
     }
 }
---
+++ star-dash/star-dash/Entities/GameEntities/Tool.swift
@@ -0,0 +1,38 @@
+//
+//  Tool.swift
+//  star-dash
+//
+//  Created by Lau Rui han on 12/3/24.
+//
+
+import Foundation
+
+class Tool: Entity {
+    let id: EntityId
+    private let position: CGPoint
+    
+    init(id: EntityId, position: CGPoint) {
+        self.id = id
+        self.position = position
+    }
+    
+    convenience init(position: CGPoint) {
+        self.init(id: UUID(), position: position)
+    }
+    
+    
+    func setUpAndAdd(to: EntityManager) {
+        let positionComponent = PositionComponent(entityId: self.id, position: self.position, rotation: .zero)
+        let physicsComponent = PhysicsComponent(
+            entityId: self.id,
+            mass: .zero,
+            velocity: .zero,
+            force: .zero,
+            collisionMask: PhysicsConstants.CollisionMask.toolCollisionMask,
+            affectedByGravity: false)
+        
+        to.add(entity: self)
+        to.add(component: positionComponent)
+        to.add(component: physicsComponent)
+    }
+}
