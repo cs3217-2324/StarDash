123717d9756b67d553f0d82bba891c1069a28bb2
Goh Jun Yi <54541329+Junyi00@users.noreply.github.com>
2024-03-12 21:43:42 +0800

    feat: Add joystick control and gestures

--- star-dash/star-dash/Rendering/ControlView.swift
+++ star-dash/star-dash/Rendering/ControlView.swift
@@ -1,14 +1,44 @@
 import UIKit
 
 class ControlView: UIView {
+    
+    var joystickControl: UIView?
+    var joystickBackground: UIView?
+    
+    let panThreshold: CGFloat = 15
 
     func setupSubviews() {
         setupMovementControls()
         setupActionControls()
+        setupGestureRecognizers()
     }
 
     private func setupMovementControls() {
+        let joystickView = UIView()
         
+        let buttonMargin: CGFloat = 50
+        let buttonWidth: CGFloat = 256
+        let buttonHeight: CGFloat = 100
+        let buttonY = frame.height - buttonHeight - buttonMargin
+        joystickView.frame = CGRect(x: buttonMargin, y: buttonY, width: buttonWidth, height: buttonHeight)
+        
+        let joystickBackground = UIImageView(image: #imageLiteral(resourceName: "JoystickBackground"))
+        joystickBackground.frame = joystickView.bounds
+        joystickBackground.alpha = 0.5
+        joystickView.addSubview(joystickBackground)
+        self.joystickBackground = joystickBackground
+        
+        let joystickControl = UIImageView(image: #imageLiteral(resourceName: "JoystickControl"))
+        joystickControl.frame = CGRect(
+            x: joystickView.frame.width / 2 - buttonHeight / 2,
+            y: joystickView.frame.height / 2 - buttonHeight / 2,
+            width: buttonHeight,
+            height: buttonHeight
+        )
+        joystickView.addSubview(joystickControl)
+        self.joystickControl = joystickControl
+        
+        addSubview(joystickView)
     }
 
     private func setupActionControls() {
@@ -26,8 +56,79 @@ class ControlView: UIView {
         jumpButton.setImage(#imageLiteral(resourceName: "JumpButtonDown"), for: .highlighted)
         addSubview(jumpButton)
     }
+    
+    private func setupGestureRecognizers() {
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan(_:)))
+        addGestureRecognizer(panGesture)
+    }
 
     @objc func jumpButtonTapped() {
         print("Tapped")
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        
+        guard let location = touches.first?.location(in: joystickBackground),
+              touches.count == 1 else {
+            return
+        }
+        
+        moveJoystick(location: location)
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesEnded(touches, with: event)
+        
+        guard let location = touches.first?.location(in: self) else {
+            return
+        }
+        
+        returnJoystick()
+    }
+    
+    @objc func handlePan(_ gesture: UIPanGestureRecognizer) {
+        if (gesture.state == .ended) {
+            returnJoystick()
+        } else {
+            moveJoystick(location: gesture.location(in: joystickBackground))
+        }
+    }
+    
+    private func moveJoystick(location: CGPoint) {
+        guard let joystickControl = self.joystickControl,
+              let joystickBackground = self.joystickBackground else {
+            return
+        }
+        
+        UIView.cancelPreviousPerformRequests(withTarget: joystickControl)
+        
+        let midPoint = joystickBackground.center
+        if shouldSendMoveEvent(location: location) {
+            // send move event
+        }
+        
+        let maxX = joystickBackground.frame.width - joystickControl.frame.width / 2
+        let minX = joystickControl.frame.width / 2
+        
+        joystickControl.center = CGPoint(
+            x: min(max(location.x, minX), maxX),
+            y: joystickControl.center.y
+        )
+    }
+    
+    private func returnJoystick() {
+        guard let joystickControl = joystickControl,
+              let joystickBackground = joystickBackground else {
+            return
+        }
+
+        UIView.animate(withDuration: 0.8, delay: 0, options: .curveEaseOut, animations: {
+            joystickControl.center = joystickBackground.center
+        })
+    }
+    
+    private func shouldSendMoveEvent(location: CGPoint) -> Bool {
+        false
     }
 }
