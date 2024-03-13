c05a4db1d3b6fa479b2242ebf63952ce3a498e12
Goh Jun Yi <54541329+Junyi00@users.noreply.github.com>
2024-03-13 12:43:41 +0800

    feat: Add overlay view

--- star-dash/star-dash/Rendering/MTKRenderer/OverlayView.swift
+++ star-dash/star-dash/Rendering/MTKRenderer/OverlayView.swift
@@ -1,6 +1,21 @@
+import UIKit
 
+class OverlayView: UIView {
 
-class OverlayView {
-    
-    
+    let margin: CGFloat = 50
+
+    let scoreLabel: UILabel = UILabel()
+
+    func setupSubviews() {
+        scoreLabel.text = "Score: 0"
+        scoreLabel.numberOfLines = 1
+        scoreLabel.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(scoreLabel)
+
+        NSLayoutConstraint.activate([
+            scoreLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: margin),
+            scoreLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -1 * margin),
+            scoreLabel.leadingAnchor.constraint(greaterThanOrEqualTo: self.leadingAnchor, constant: margin)
+        ])
+    }
 }
--- star-dash/star-dash/Rendering/MTKRenderer/PlayerView.swift
+++ star-dash/star-dash/Rendering/MTKRenderer/PlayerView.swift
@@ -4,11 +4,15 @@ import MetalKit
 class PlayerView {
     var sceneView: MTKView
     var controlView: ControlView
+    var overlayView: OverlayView
 
     init(superview: UIView, device: MTLDevice, drawDelegate: MTKViewDelegate) {
         self.sceneView = MTKView(frame: superview.frame, device: device)
         self.sceneView.delegate = drawDelegate
         superview.addSubview(self.sceneView)
+
+        self.overlayView = OverlayView(frame: superview.frame)
+        superview.addSubview(self.overlayView)
 
         self.controlView = ControlView(frame: superview.frame)
         superview.addSubview(self.controlView)
