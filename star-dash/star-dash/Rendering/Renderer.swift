e79105d4d635ae40d876a13b7d95cd87bbbe7619
ruihan00 <ruixhan@gmail.com>
2024-03-14 00:35:24 +0800

    modify linter to use docker image

--- star-dash/star-dash/Rendering/Renderer.swift
+++
@@ -1,8 +0,0 @@
-import UIKit
-
-/**
- The `Renderer` protocol defines the requirements for an object responsible for rendering game objects onto a view.
- */
-protocol Renderer {
-    func createSinglePlayerView(at rootView: UIView)
-}
