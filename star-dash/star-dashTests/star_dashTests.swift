5fa3b64893a1f176d930833e90f396bc067a8da6
ruihan00 <ruixhan@gmail.com>
2024-03-14 01:07:59 +0800

    comment on testcases

--- star-dash/star-dashTests/star_dashTests.swift
+++ star-dash/star-dashTests/star_dashTests.swift
@@ -27,11 +27,11 @@ final class star_dashTests: XCTestCase {
     //        // assertions afterwards.
     //    }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+    // func testPerformanceExample() throws {
+    //     // This is an example of a performance test case.
+    //     self.measure {
+    //         // Put the code you want to measure the time of here.
+    //     }
+    // }
 
 }
