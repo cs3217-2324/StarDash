65f83e42730c3cb6826d474ddcd31385833aa201
Goh Jun Yi <54541329+Junyi00@users.noreply.github.com>
2024-03-13 15:45:56 +0800

    chore: Refactor JoystickView

--- star-dash/star-dash/Rendering/MTKRenderer/JoystickView.swift
+++ star-dash/star-dash/Rendering/MTKRenderer/JoystickView.swift
@@ -10,22 +10,10 @@ class JoystickView: UIView {
     let joystickControl: UIView
 
     init(frame: CGRect, buttonSize: CGFloat) {
-        self.joystickBackground = UIImageView(image: #imageLiteral(resourceName: "JoystickBackground"))
-        joystickBackground.frame = CGRect(
-            x: 0, y: 0, width: frame.width, height: frame.height
-        )
-        joystickBackground.alpha = 0.5
-        
-        self.joystickControl = UIImageView(image: #imageLiteral(resourceName: "JoystickControl"))
-        joystickControl.frame = CGRect(
-            x: frame.width / 2 - buttonSize / 2,
-            y: frame.height / 2 - buttonSize / 2,
-            width: buttonSize,
-            height: buttonSize
-        )
-        
+        self.joystickBackground = createJoystickBackgroundView(frame: frame)
+        self.joystickControl = createJoystickControlView(frame: frame, buttonSize: buttonSize)
         super.init(frame: frame)
-        
+
         addSubview(joystickBackground)
         addSubview(joystickControl)
     }
@@ -43,10 +31,10 @@ class JoystickView: UIView {
         if shouldSendMoveEvent(location: location) {
             // send move event
         }
-        
+
         let maxX = joystickBackground.frame.width - joystickControl.frame.width / 2
         let minX = joystickControl.frame.width / 2
-        
+
         joystickControl.center = CGPoint(
             x: min(max(location.x, minX), maxX),
             y: joystickControl.center.y
@@ -58,6 +46,30 @@ class JoystickView: UIView {
         UIView.animate(withDuration: 0.8, delay: 0, options: .curveEaseOut, animations: {
             self.joystickControl.center = self.joystickBackground.center
         })
+    }
+
+    // MARK: private methods
+
+    private func createJoystickBackgroundView(frame: CGRect) -> UIView {
+        let view = UIImageView(image: #imageLiteral(resourceName: "JoystickBackground"))
+        joystickBackground.frame = CGRect(
+            x: 0, y: 0, width: frame.width, height: frame.height
+        )
+        joystickBackground.alpha = 0.5
+
+        return view
+    }
+
+    private func createJoystickControlView(frame: CGRect, buttonSize: CGFloat) -> UIView {
+        let view = UIImageView(image: #imageLiteral(resourceName: "JoystickControl"))
+        joystickControl.frame = CGRect(
+            x: frame.width / 2 - buttonSize / 2,
+            y: frame.height / 2 - buttonSize / 2,
+            width: buttonSize,
+            height: buttonSize
+        )
+
+        return view
     }
     
     /// Returns true if the joystick has moved significantly enough
