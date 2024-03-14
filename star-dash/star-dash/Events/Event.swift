a0e1e4e36dc2efb4ab4756acc35f257aa496211d
jasonqiu212 <jason_qiu@hotmail.com>
2024-03-14 14:01:14 +0800

    feat: add mock event

--- star-dash/star-dash/Events/Event.swift
+++ star-dash/star-dash/Events/Event.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 protocol Event {
-    var timestamp: TimeInterval { get }
+    var timestamp: Date { get }
     var entityId: EntityId { get }
 
     func execute(on target: EventModifiable)
