e4b48118b5db2093e682a366e73ade5958e7f11f
jasonqiu212 <jason_qiu@hotmail.com>
2024-03-16 04:04:31 +0800

    feat: add CollectiblePickupEvent

---
+++ star-dash/star-dash/Events/CollectiblePickupEvent.swift
@@ -0,0 +1,25 @@
+//
+//  CollectiblePickupEvent.swift
+//  star-dash
+//
+//  Created by Jason Qiu on 16/3/24.
+//
+
+import Foundation
+
+class CollectiblePickupEvent: Event {
+    let timestamp: Date
+    let entityId: EntityId
+    
+    let collectibleEntityId: EntityId
+    
+    init(by entityId: EntityId, collectibleEntityId: EntityId) {
+        timestamp = Date.now
+        self.entityId = entityId
+        self.collectibleEntityId = collectibleEntityId
+    }
+    
+    func execute(on target: EventModifiable) {
+        // TODO: Get inventory system/score system and update accordingly
+    }
+}
