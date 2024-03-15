d79d5866549aff3dfa19831ed75a2e4a77675470
jasonqiu212 <jason_qiu@hotmail.com>
2024-03-16 03:54:38 +0800

    feat: add mock PlayerMonsterCollisionEvent

---
+++ star-dash/star-dash/Events/PlayerMonsterCollisionEvent.swift
@@ -0,0 +1,26 @@
+//
+//  PlayerMonsterCollisionEvent.swift
+//  star-dash
+//
+//  Created by Jason Qiu on 16/3/24.
+//
+
+import Foundation
+
+class PlayerMonsterCollisionEvent: Event {
+    let timestamp: Date
+    let entityId: EntityId
+
+    let monsterEntityId: EntityId
+
+    init(from playerEntityId: EntityId, on monsterEntityId: EntityId) {
+        self.timestamp = Date.now
+        entityId = playerEntityId
+        self.monsterEntityId = monsterEntityId
+    }
+
+    func execute(on target: EventModifiable) {
+        // TODO: Determine if it's player attacking monster or vice versa.
+        // Trigger HurtByMonsterEvent and AttackMonsterEvent correspondingly
+    }
+}
