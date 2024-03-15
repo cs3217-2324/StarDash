7ea238e6541f04a1c9a968e37ea6aeeb76381dbf
jasonqiu212 <jason_qiu@hotmail.com>
2024-03-16 03:22:47 +0800

    feat: add JumpEvent

---
+++ star-dash/star-dash/Events/JumpEvent.swift
@@ -0,0 +1,31 @@
+//
+//  JumpEvent.swift
+//  star-dash
+//
+//  Created by Jason Qiu on 16/3/24.
+//
+
+import Foundation
+
+class JumpEvent: Event {
+    let entityId: EntityId
+    let timestamp: Date
+
+    let jumpImpulse: CGVector
+
+    init(entityId: EntityId, jumpImpulse: CGVector, timestamp: Date = Date.now) {
+        self.entityId = entityId
+        self.timestamp = timestamp
+        self.jumpImpulse = jumpImpulse
+    }
+
+    func execute(on target: EventModifiable) {
+        guard let physicsSystem = target.system(ofType: PhysicsSystem.self) else {
+            return
+        }
+        guard !physicsSystem.isJumping(entityId) else {
+            return
+        }
+        physicsSystem.applyImpulse(to: entityId, impulse: jumpImpulse)
+    }
+}
