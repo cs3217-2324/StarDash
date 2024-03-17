7b684f6b1625e235f06029e58a756d64b00c0b12
jasonqiu212 <jason_qiu@hotmail.com>
2024-03-17 12:25:45 +0800

    feat: add health system

---
+++ star-dash/star-dash/GameEngine/Systems/HealthSystem.swift
@@ -0,0 +1,40 @@
+//
+//  HealthSystem.swift
+//  star-dash
+//
+//  Created by Jason Qiu on 17/3/24.
+//
+
+import Foundation
+
+class HealthSystem: System {
+    var isActive: Bool
+    var dispatcher: EventModifiable?
+    var entityManager: EntityManager
+
+    init(_ entityManager: EntityManager, dispatcher: EventModifiable? = nil) {
+        self.isActive = true
+        self.entityManager = entityManager
+        self.dispatcher = dispatcher
+    }
+
+    func increaseHealth(of entityId: EntityId, by increment: Int) {
+        guard let healthComponent = getHealthComponent(of: entityId) else {
+            return
+        }
+
+        healthComponent.health += increment
+    }
+    
+    func decreaseHealth(of entityId: EntityId, by decrement: Int) {
+        guard let healthComponent = getHealthComponent(of: entityId) else {
+            return
+        }
+
+        healthComponent.health -= decrement
+    }
+
+    private func getHealthComponent(of entityId: EntityId) -> HealthComponent? {
+        entityManager.component(ofType: HealthComponent.self, of: entityId)
+    }
+}
