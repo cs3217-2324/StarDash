cd747c3e23b1e87762f2ec94cf7855063a6241cc
Goh Jun Yi <54541329+Junyi00@users.noreply.github.com>
2024-03-27 16:22:49 +0800

    chore: Add docs for UIView extension

--- star-dash/star-dash/Extensions/UIView.swift
+++ star-dash/star-dash/Extensions/UIView.swift
@@ -1,5 +1,9 @@
 import UIKit
 
+/**
+ * The extension introduces an initisalisation method to add subviews
+ * to a frame that is rotated, however the view is not rotated.
+ */
 extension UIView {
     convenience init(frame: CGRect, rotatedBy rotation: CGFloat) {
         self.init(frame: frame.applying(CGAffineTransform(rotationAngle: rotation)))
