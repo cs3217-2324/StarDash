d12ba4f878b038d70e612f86d1d850afa3851a06
HoJunHao2000 <kennethhojunhao@gmail.com>
2024-03-28 10:39:02 +0800

    Add EntityBuilder

--- star-dash/star-dash/GameEngine/Entities/EntityFactory.swift
+++ star-dash/star-dash/GameEngine/Entities/EntityFactory.swift
@@ -12,34 +12,24 @@ struct EntityFactory {
                                    playerIndex: Int,
                                    position: CGPoint,
                                    sprite: PlayerSprite) {
-        let id = UUID()
-        let player = Player(id: id)
+        let playerBuilder = EntityBuilder(entity: Player(id: UUID()), entityManager: entityManager)
 
-        let playerComponent = PlayerComponent(entityId: id, playerIndex: playerIndex)
-        let positionComponent = PositionComponent(entityId: id, position: position, rotation: .zero)
-        let healthComponent = HealthComponent(entityId: id, health: GameConstants.InitialHealth.player)
-        let spriteComponent = SpriteComponent(
-            entityId: id,
-            image: SpriteConstants.PlayerRedNose,
-            textureSet: SpriteConstants.PlayerRedNoseTexture,
-            textureAtlas: nil,
-            size: PhysicsConstants.Dimensions.player
-        )
-        let scoreComponent = ScoreComponent(entityId: id, score: 0)
-        let physicsComponent = PhysicsComponent(entityId: id, size: PhysicsConstants.Dimensions.player)
-        physicsComponent.categoryBitMask = PhysicsConstants.CollisionCategory.player
-        physicsComponent.contactTestMask = PhysicsConstants.ContactMask.player
-        physicsComponent.collisionBitMask = PhysicsConstants.CollisionMask.player
-        physicsComponent.affectedByGravity = true
-        physicsComponent.restitution = 0.0
-
-        entityManager.add(entity: player)
-        entityManager.add(component: playerComponent)
-        entityManager.add(component: positionComponent)
-        entityManager.add(component: healthComponent)
-        entityManager.add(component: physicsComponent)
-        entityManager.add(component: spriteComponent)
-        entityManager.add(component: scoreComponent)
+        playerBuilder
+            .withPlayer(playerIndex: playerIndex)
+            .withPosition(at: position)
+            .withHealth(health: GameConstants.InitialHealth.player)
+            .withSprite(image: SpriteConstants.PlayerRedNose,
+                        textureSet: SpriteConstants.PlayerRedNoseTexture,
+                        textureAtlas: nil,
+                        size: PhysicsConstants.Dimensions.player)
+            .withScore(score: 0)
+            .withPhysics(size: PhysicsConstants.Dimensions.player)
+                .configureCategoryBitMask(PhysicsConstants.CollisionCategory.player)
+                .configureContactTestMask(PhysicsConstants.ContactMask.player)
+                .configureCollisionBitMask(PhysicsConstants.CollisionMask.player)
+                .configureAffectedByGravity(true)
+                .configureRestitution(0.0)
+            .addToGame()
     }
 
     static func createAndAddMonster(to entityManager: EntityManagerInterface,
@@ -47,24 +37,18 @@ struct EntityFactory {
                                     health: Int,
                                     sprite: String,
                                     size: CGSize) {
-        let id = UUID()
-        let monster = Monster(id: id)
+        let monsterBuilder = EntityBuilder(entity: Monster(id: UUID()), entityManager: entityManager)
 
-        let positionComponent = PositionComponent(entityId: id, position: position, rotation: .zero)
-        let healthComponent = HealthComponent(entityId: id, health: health)
-        let spriteComponent = SpriteComponent(entityId: id,
-                                              image: sprite,
-                                              textureSet: nil,
-                                              textureAtlas: nil,
-                                              size: size)
-        let physicsComponent = PhysicsComponent(entityId: id, size: PhysicsConstants.Dimensions.monster)
-        physicsComponent.collisionBitMask = PhysicsConstants.CollisionMask.monster
-
-        entityManager.add(entity: monster)
-        entityManager.add(component: positionComponent)
-        entityManager.add(component: healthComponent)
-        entityManager.add(component: physicsComponent)
-        entityManager.add(component: spriteComponent)
+        monsterBuilder
+            .withPosition(at: position)
+            .withHealth(health: health)
+            .withSprite(image: sprite,
+                        textureSet: nil,
+                        textureAtlas: nil,
+                        size: size)
+            .withPhysics(size: PhysicsConstants.Dimensions.monster)
+                .configureCollisionBitMask(PhysicsConstants.CollisionMask.monster)
+            .addToGame()
     }
 
     static func createAndAddCollectible(to entityManager: EntityManagerInterface,
@@ -72,105 +56,81 @@ struct EntityFactory {
                                         sprite: String,
                                         points: Int,
                                         size: CGSize) {
-        let id = UUID()
-        let collectible = Collectible(id: id)
+        let collectibleBuilder = EntityBuilder(entity: Collectible(id: UUID()), entityManager: entityManager)
 
-        let positionComponent = PositionComponent(entityId: id, position: position, rotation: .zero)
-        let spriteComponent = SpriteComponent(
-            entityId: id,
-            image: sprite,
-            textureSet: nil,
-            textureAtlas: nil,
-            size: size
-        )
-        let pointsComponent = PointsComponent(entityId: id, points: points)
-        let physicsComponent = PhysicsComponent(entityId: id, size: size)
-        physicsComponent.affectedByGravity = false
-        physicsComponent.isDynamic = false
-        physicsComponent.categoryBitMask = PhysicsConstants.CollisionCategory.collectible
-        physicsComponent.contactTestMask = PhysicsConstants.ContactMask.collectible
-        physicsComponent.collisionBitMask = PhysicsConstants.CollisionMask.collectible
-
-        entityManager.add(entity: collectible)
-        entityManager.add(component: positionComponent)
-        entityManager.add(component: physicsComponent)
-        entityManager.add(component: spriteComponent)
-        entityManager.add(component: pointsComponent)
+        collectibleBuilder
+            .withPosition(at: position)
+            .withSprite(image: sprite,
+                        textureSet: nil,
+                        textureAtlas: nil,
+                        size: size)
+            .withPoint(points: points)
+            .withPhysics(size: size)
+                .configureCategoryBitMask(PhysicsConstants.CollisionCategory.collectible)
+                .configureContactTestMask(PhysicsConstants.ContactMask.collectible)
+                .configureCollisionBitMask(PhysicsConstants.CollisionMask.collectible)
+                .configureAffectedByGravity(false)
+                .configureIsDynamic(false)
+            .addToGame()
     }
 
     static func createAndAddObstacle(to entityManager: EntityManagerInterface,
                                      position: CGPoint,
                                      sprite: String,
                                      size: CGSize) {
-        let id = UUID()
-        let obstacle = Obstacle(id: id)
+        let obstacleBuilder = EntityBuilder(entity: Obstacle(id: UUID()), entityManager: entityManager)
 
-        let positionComponent = PositionComponent(entityId: id, position: position, rotation: .zero)
-        let spriteComponent = SpriteComponent(entityId: id,
-                                              image: sprite,
-                                              textureSet: nil,
-                                              textureAtlas: nil,
-                                              size: size)
-        let physicsComponent = PhysicsComponent(entityId: id, size: size)
-        physicsComponent.collisionBitMask = PhysicsConstants.CollisionMask.obstacle
-        physicsComponent.isDynamic = false
-
-        entityManager.add(entity: obstacle)
-        entityManager.add(component: positionComponent)
-        entityManager.add(component: physicsComponent)
-        entityManager.add(component: spriteComponent)
+        obstacleBuilder
+            .withPosition(at: position)
+            .withSprite(image: sprite,
+                        textureSet: nil,
+                        textureAtlas: nil,
+                        size: size)
+            .withPhysics(size: size)
+                .configureCollisionBitMask(PhysicsConstants.CollisionMask.obstacle)
+                .configureIsDynamic(false)
+            .addToGame()
     }
 
     static func createAndAddTool(to entityManager: EntityManagerInterface,
                                  position: CGPoint,
                                  sprite: String,
                                  size: CGSize) {
-        let id = UUID()
-        let tool = Tool(id: id)
+        let toolBuilder = EntityBuilder(entity: Tool(id: UUID()), entityManager: entityManager)
 
-        let positionComponent = PositionComponent(entityId: id, position: position, rotation: .zero)
-        let spriteComponent = SpriteComponent(entityId: id,
-                                              image: sprite,
-                                              textureSet: nil,
-                                              textureAtlas: nil,
-                                              size: size)
-        let physicsComponent = PhysicsComponent(entityId: id, size: size)
-        physicsComponent.collisionBitMask = PhysicsConstants.CollisionMask.tool
-        physicsComponent.isDynamic = false
-
-        entityManager.add(entity: tool)
-        entityManager.add(component: positionComponent)
-        entityManager.add(component: physicsComponent)
-        entityManager.add(component: spriteComponent)
+        toolBuilder
+            .withPosition(at: position)
+            .withSprite(image: sprite,
+                        textureSet: nil,
+                        textureAtlas: nil,
+                        size: size)
+            .withPhysics(size: size)
+                .configureCollisionBitMask(PhysicsConstants.CollisionMask.tool)
+                .configureIsDynamic(false)
+            .addToGame()
     }
 
     static func createAndAddWall(to entityManager: EntityManagerInterface, position: CGPoint, size: CGSize) {
-        let id = UUID()
-        let wall = Wall(id: id)
+        let wallBuilder = EntityBuilder(entity: Wall(id: UUID()), entityManager: entityManager)
 
-        let positionComponent = PositionComponent(entityId: id, position: position, rotation: .zero)
-        let physicsComponent = PhysicsComponent(entityId: id, size: PhysicsConstants.Dimensions.wall)
-        physicsComponent.collisionBitMask = PhysicsConstants.CollisionMask.wall
-
-        entityManager.add(entity: wall)
-        entityManager.add(component: positionComponent)
-        entityManager.add(component: physicsComponent)
+        wallBuilder
+            .withPosition(at: position)
+            .withPhysics(size: size)
+                .configureCollisionBitMask(PhysicsConstants.CollisionMask.wall)
+            .addToGame()
     }
 
     static func createAndAddFloor(to entityManager: EntityManagerInterface, position: CGPoint, size: CGSize) {
-        let id = UUID()
-        let floor = Floor(id: id)
+        let floorBuilder = EntityBuilder(entity: Floor(id: UUID()), entityManager: entityManager)
 
-        let positionComponent = PositionComponent(entityId: id, position: position, rotation: .zero)
-        let physicsComponent = PhysicsComponent(entityId: id, size: size)
-        physicsComponent.restitution = 0.0
-        physicsComponent.isDynamic = false
-        physicsComponent.categoryBitMask = PhysicsConstants.CollisionCategory.floor
-        physicsComponent.contactTestMask = PhysicsConstants.ContactMask.floor
-        physicsComponent.collisionBitMask = PhysicsConstants.CollisionMask.floor
-
-        entityManager.add(entity: floor)
-        entityManager.add(component: positionComponent)
-        entityManager.add(component: physicsComponent)
+        floorBuilder
+            .withPosition(at: position)
+            .withPhysics(size: size)
+                .configureCategoryBitMask(PhysicsConstants.CollisionCategory.floor)
+                .configureContactTestMask(PhysicsConstants.ContactMask.floor)
+                .configureCollisionBitMask(PhysicsConstants.CollisionMask.floor)
+                .configureIsDynamic(false)
+                .configureRestitution(0.0)
+            .addToGame()
     }
 }
