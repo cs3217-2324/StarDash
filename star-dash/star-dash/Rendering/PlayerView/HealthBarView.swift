faa95554d014f22b8445b77dd1037e6d111c9896
Goh Jun Yi <54541329+Junyi00@users.noreply.github.com>
2024-04-15 02:11:19 +0800

    feat: Healthbar

--- star-dash/star-dash/Rendering/PlayerView/HealthBarView.swift
+++ star-dash/star-dash/Rendering/PlayerView/HealthBarView.swift
@@ -1,34 +1,50 @@
 import UIKit
 
 class HealthBarView: UIView {
-    private var healthBar: UIView = UIView()
-    
+    private var healthBar = UIView()
+    private var maxHealthBar = UIView()
+
     var maxHealth: CGFloat = 100.0 {
         didSet {
             setNeedsLayout()
         }
     }
-    
+
     var currentHealth: CGFloat = 100.0 {
         didSet {
             updateHealthBar()
         }
     }
-    
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupHealthBar()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupHealthBar()
+    }
+
     private func setupHealthBar() {
+        maxHealthBar.layer.borderWidth = 2
+        maxHealthBar.layer.borderColor = UIColor.green.cgColor
+        addSubview(maxHealthBar)
+
         healthBar.backgroundColor = UIColor.green
         addSubview(healthBar)
     }
-    
+
     private func updateHealthBar() {
         let healthPercentage = currentHealth / maxHealth
         let newWidth = bounds.width * healthPercentage
         healthBar.frame.size.width = newWidth
     }
-    
+
     override func layoutSubviews() {
         super.layoutSubviews()
         healthBar.frame = CGRect(x: 0, y: 0, width: bounds.width, height: bounds.height)
+        maxHealthBar.frame = CGRect(x: 0, y: 0, width: bounds.width, height: bounds.height)
         updateHealthBar()
     }
 }
--- star-dash/star-dash/Rendering/PlayerView/OverlayView.swift
+++ star-dash/star-dash/Rendering/PlayerView/OverlayView.swift
@@ -9,7 +9,6 @@ class OverlayView: UIView {
     let margin: CGFloat = 50
 
     let scoreLabel = UILabel()
-    // let healthLabel = UILabel()
     let healthBarView = HealthBarView()
 
     func setupSubviews() {
@@ -18,24 +17,22 @@ class OverlayView: UIView {
         scoreLabel.textColor = .black
         addSubview(scoreLabel)
 
-        // healthLabel.numberOfLines = 1
-        // healthLabel.translatesAutoresizingMaskIntoConstraints = false
-        // healthLabel.textColor = .black
+        healthBarView.translatesAutoresizingMaskIntoConstraints = false
         addSubview(healthBarView)
 
         NSLayoutConstraint.activate([
-            scoreLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: margin),
-            scoreLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -1 * margin),
-            scoreLabel.leadingAnchor.constraint(greaterThanOrEqualTo: self.leadingAnchor, constant: margin)
+            healthBarView.topAnchor.constraint(equalTo: self.topAnchor, constant: margin),
+            healthBarView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -1 * margin),
+            healthBarView.widthAnchor.constraint(equalToConstant: 300),
+            healthBarView.heightAnchor.constraint(equalToConstant: 50)
         ])
 
         NSLayoutConstraint.activate([
-            healthBarView.topAnchor.constraint(equalTo: scoreLabel.topAnchor, constant: margin),
-            healthBarView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -1 * margin),
-            healthBarView.leadingAnchor.constraint(greaterThanOrEqualTo: self.leadingAnchor, constant: margin)
+            scoreLabel.topAnchor.constraint(equalTo: healthBarView.bottomAnchor, constant: margin),
+            scoreLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -1 * margin),
+            scoreLabel.leadingAnchor.constraint(greaterThanOrEqualTo: self.leadingAnchor, constant: margin)
         ])
-
-        update(score: 0, health: 0)
+        update(score: 0, health: 100)
     }
 
     func update(score: Int, health: Int) {
