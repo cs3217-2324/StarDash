a0dae4f9bf7f1a80ccbc3396be23bb84df69f099
jasonqiu212 <jason_qiu@hotmail.com>
2024-04-16 20:50:20 +0800

    refactor: refactor game mode specific constants to new file

---
+++ star-dash/star-dash/Constants/GameModeConstants.swift
@@ -0,0 +1,17 @@
+//
+//  GameModeConstants.swift
+//  star-dash
+//
+//  Created by Jason Qiu on 16/4/24.
+//
+
+import Foundation
+
+struct GameModeConstants {
+    struct LocalRaceMode {
+        static let rankingScoreChangeMap = [
+            1: 100,
+            2: 50
+        ]
+    }
+}
